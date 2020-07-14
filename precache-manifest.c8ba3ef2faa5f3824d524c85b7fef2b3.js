a4c86b179cc516499b35a22168356ac57eb2c407
lunann <annika.birgitta@hotmail.com>
2020-07-14 19:00:57 +0200

    Updates

--- precache-manifest.c8ba3ef2faa5f3824d524c85b7fef2b3.js
+++ precache-manifest.c8ba3ef2faa5f3824d524c85b7fef2b3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f157846d86edd7a370aa3993c22bef55",
+    "revision": "88778716b12e6065f9978f732acf1bfa",
     "url": "/thecreativegoose/index.html"
   },
   {
-    "revision": "a709379f8059c63b0ba2",
+    "revision": "108440892b94c3ea7504",
     "url": "/thecreativegoose/static/css/2.aec6ccec.chunk.css"
   },
   {
-    "revision": "9ae27ab83a8a1d02d113",
+    "revision": "15025c2e1a3eedf141b8",
     "url": "/thecreativegoose/static/css/main.9085d3b7.chunk.css"
   },
   {
-    "revision": "a709379f8059c63b0ba2",
-    "url": "/thecreativegoose/static/js/2.5fe6ba01.chunk.js"
+    "revision": "108440892b94c3ea7504",
+    "url": "/thecreativegoose/static/js/2.b7d2ce17.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/thecreativegoose/static/js/2.5fe6ba01.chunk.js.LICENSE.txt"
+    "url": "/thecreativegoose/static/js/2.b7d2ce17.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9ae27ab83a8a1d02d113",
-    "url": "/thecreativegoose/static/js/main.97f5f4ec.chunk.js"
+    "revision": "15025c2e1a3eedf141b8",
+    "url": "/thecreativegoose/static/js/main.414a0e54.chunk.js"
   },
   {
     "revision": "d4c1545282a78c3b7f39",
