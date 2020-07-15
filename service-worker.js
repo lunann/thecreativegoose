1f35df6e90d56aee304428685cebaad47c710537
lunann <annika.birgitta@hotmail.com>
2020-07-16 01:02:35 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,11 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
+<<<<<<< HEAD
   "/thecreativegoose/precache-manifest.c8ba3ef2faa5f3824d524c85b7fef2b3.js"
+=======
+  "/thecreativegoose/precache-manifest.110ce379c1c2470e2dbb047fb0b8bd0b.js"
+>>>>>>> 708d3ef5... Updates
 );
 
 self.addEventListener('message', (event) => {
