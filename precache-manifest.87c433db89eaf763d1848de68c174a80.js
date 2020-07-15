f2b34bc884c9213ecfe7d0790cc5c77a73f46bb9
lunann <annika.birgitta@hotmail.com>
2020-07-15 14:00:14 +0200

    Updates

---
+++ precache-manifest.87c433db89eaf763d1848de68c174a80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c8f39e11c15b94c624d2f055b4eea2",
+    "url": "/thecreativegoose/index.html"
+  },
+  {
+    "revision": "a4c53914133e3de01c05",
+    "url": "/thecreativegoose/static/css/2.046e01ea.chunk.css"
+  },
+  {
+    "revision": "96db0ff4cd95281b2fa5",
+    "url": "/thecreativegoose/static/css/main.4ff8caa8.chunk.css"
+  },
+  {
+    "revision": "a4c53914133e3de01c05",
+    "url": "/thecreativegoose/static/js/2.50e20497.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/thecreativegoose/static/js/2.50e20497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96db0ff4cd95281b2fa5",
+    "url": "/thecreativegoose/static/js/main.5eb621e5.chunk.js"
+  },
+  {
+    "revision": "d4c1545282a78c3b7f39",
+    "url": "/thecreativegoose/static/js/runtime-main.dbc200b3.js"
+  },
+  {
+    "revision": "3149e315c6e428d4dffcbc4d6c884796",
+    "url": "/thecreativegoose/static/media/annikalundberg.3149e315.pdf"
+  }
+]);
