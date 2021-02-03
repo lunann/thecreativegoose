1929cf502f8ecb6f9693b5dfac9e416a0c13576a
lunann <annika.birgitta@hotmail.com>
2021-02-03 15:36:14 +0100

    Update

---
+++ build/precache-manifest.18f3d170a9680ab3c0cee5ab689e7988.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0cfa7f05decf2d8090c08c7fa62d74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01b628c471e12b4d5a30",
+    "url": "./static/css/2.ea4ca4ef.chunk.css"
+  },
+  {
+    "revision": "9df88604f059bfe2750c",
+    "url": "./static/css/main.12f3c275.chunk.css"
+  },
+  {
+    "revision": "01b628c471e12b4d5a30",
+    "url": "./static/js/2.3d1ae921.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.3d1ae921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df88604f059bfe2750c",
+    "url": "./static/js/main.e59ad72a.chunk.js"
+  },
+  {
+    "revision": "be44b3bd2559320e734a",
+    "url": "./static/js/runtime-main.1ac7b0ef.js"
+  },
+  {
+    "revision": "2e02909310ce3d2912f85ab252168662",
+    "url": "./static/media/36.2e029093.jpg"
+  },
+  {
+    "revision": "88f475b7d1dcdba95ca3315bf56981f4",
+    "url": "./static/media/36palette.88f475b7.jpg"
+  },
+  {
+    "revision": "7c18a689456cafe83e5debf9cbc714b3",
+    "url": "./static/media/9.7c18a689.jpg"
+  },
+  {
+    "revision": "f115d58ebc2541d61d752246632abb61",
+    "url": "./static/media/D.f115d58e.jpg"
+  },
+  {
+    "revision": "ffbd3d83ce94a5e99a4aeb00b68d4cc1",
+    "url": "./static/media/H.ffbd3d83.jpg"
+  },
+  {
+    "revision": "2dd4b1ebc0de2ee2ac3d814b9fa56ffb",
+    "url": "./static/media/Q.2dd4b1eb.jpg"
+  },
+  {
+    "revision": "d770e825e05f8590e7cfc30d1c39c58a",
+    "url": "./static/media/annikalundberg.d770e825.pdf"
+  },
+  {
+    "revision": "4353b2720795e0de998cbd1d50edafa4",
+    "url": "./static/media/cityPopPalette.4353b272.jpg"
+  },
+  {
+    "revision": "a23064b009f4c335f249d587d28c6cff",
+    "url": "./static/media/citypopFront.a23064b0.jpg"
+  },
+  {
+    "revision": "9a2a044fdc37d61fbe90d2eac4675e2e",
+    "url": "./static/media/detail01.9a2a044f.jpg"
+  },
+  {
+    "revision": "81196969bab3822926a829c263ddd048",
+    "url": "./static/media/detail02.81196969.jpg"
+  },
+  {
+    "revision": "6beb40f73752551c87c5a54bbd11faa5",
+    "url": "./static/media/detail03.6beb40f7.jpg"
+  },
+  {
+    "revision": "2220b5cdb8f011d3ca0ea58c16ce5907",
+    "url": "./static/media/front_plant.2220b5cd.jpg"
+  },
+  {
+    "revision": "eea79c8e0e6d1a6abb47c1b44bd1031d",
+    "url": "./static/media/graphic.eea79c8e.png"
+  },
+  {
+    "revision": "d883c38807089a496e81dc34e951e663",
+    "url": "./static/media/instagramLogo.d883c388.png"
+  },
+  {
+    "revision": "eb7e54a6abb31e3578302b9c9e0bdcda",
+    "url": "./static/media/landingPage1.eb7e54a6.jpg"
+  },
+  {
+    "revision": "c3804d49dd0de2c80cc1b39d1599439e",
+    "url": "./static/media/linkedInLogo.c3804d49.png"
+  },
+  {
+    "revision": "9b06858ea452b61d28e2ec6b46c05858",
+    "url": "./static/media/livaLogo.9b06858e.jpg"
+  },
+  {
+    "revision": "fa024b1611f02af9c6f15a0eeb975c70",
+    "url": "./static/media/livaPalette.fa024b16.jpg"
+  },
+  {
+    "revision": "890e58dcb180bbbcf3a7902d64759865",
+    "url": "./static/media/livafront.890e58dc.jpg"
+  },
+  {
+    "revision": "2a4734495224fdc933fd9f6a2de9ea99",
+    "url": "./static/media/plant_1.2a473449.jpg"
+  },
+  {
+    "revision": "d10ed2125ed604a1d9bbc056375d863f",
+    "url": "./static/media/plant_2.d10ed212.jpg"
+  },
+  {
+    "revision": "ab0ad5ae289218de6e9af56969e8756c",
+    "url": "./static/media/plant_4.ab0ad5ae.jpg"
+  },
+  {
+    "revision": "987b31085eb0dda4d1bfe98a220887a6",
+    "url": "./static/media/plant_5.987b3108.JPG"
+  },
+  {
+    "revision": "8e3dd4ecdbfbe2af76de67514eb4f75d",
+    "url": "./static/media/plant_6.8e3dd4ec.jpg"
+  },
+  {
+    "revision": "87782c2b117a78db93e73f70504f2d49",
+    "url": "./static/media/plant_palette.87782c2b.jpg"
+  },
+  {
+    "revision": "0df0a367ba9e7d5678ba4b0e19c1d675",
+    "url": "./static/media/population.0df0a367.jpg"
+  },
+  {
+    "revision": "83802237eacd71552b0d4f2f3e37c275",
+    "url": "./static/media/searchByCountry.83802237.jpg"
+  },
+  {
+    "revision": "bc6b2d1071cbd15524fd57a239e0736d",
+    "url": "./static/media/searchCountry.bc6b2d10.jpg"
+  },
+  {
+    "revision": "b818d523d8560f33bcf5d6628f0ffa73",
+    "url": "./static/media/spotifyLogo.b818d523.png"
+  },
+  {
+    "revision": "982cf19f1120abae8b12cb645eda3147",
+    "url": "./static/media/thisIsMe1.982cf19f.jpg"
+  },
+  {
+    "revision": "5c19832b916777160261577a3d50e93a",
+    "url": "./static/media/topthree.5c19832b.jpg"
+  }
+]);
