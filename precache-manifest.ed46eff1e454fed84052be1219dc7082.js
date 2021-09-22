c467020ceb0e7d96f016165aba8a19b1c11597f0
lunann <annika.birgitta@hotmail.com>
2021-09-22 15:56:50 +0200

    Updates

---
+++ precache-manifest.ed46eff1e454fed84052be1219dc7082.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0e8b169f476d4f0661c6d3b6aaa81c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f8f87b472db18c0505",
+    "url": "/static/css/2.ea4ca4ef.chunk.css"
+  },
+  {
+    "revision": "0311d4f294590a56771d",
+    "url": "/static/css/main.76d8ef6a.chunk.css"
+  },
+  {
+    "revision": "c6f8f87b472db18c0505",
+    "url": "/static/js/2.90883bbc.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.90883bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0311d4f294590a56771d",
+    "url": "/static/js/main.fc547fcd.chunk.js"
+  },
+  {
+    "revision": "c25763fc421c741e4365",
+    "url": "/static/js/runtime-main.57545fcd.js"
+  },
+  {
+    "revision": "855652989723a021a0e434818591d06c",
+    "url": "/static/media/1.85565298.png"
+  },
+  {
+    "revision": "873f3cea98b72e6821a2de3373af9960",
+    "url": "/static/media/1.873f3cea.png"
+  },
+  {
+    "revision": "2795763ff0fc728fa74122746cf52b9d",
+    "url": "/static/media/2.2795763f.png"
+  },
+  {
+    "revision": "5a47a808f7bd2a967c2f6a57f9f45e50",
+    "url": "/static/media/2.5a47a808.png"
+  },
+  {
+    "revision": "ca43d8bf5ec27c8804296747296ec5f6",
+    "url": "/static/media/21.ca43d8bf.png"
+  },
+  {
+    "revision": "65ef4477f4e3c8ff6329770b9163ed6a",
+    "url": "/static/media/3.65ef4477.png"
+  },
+  {
+    "revision": "96a2719095cae33b7fd1eaf2fe160552",
+    "url": "/static/media/3.96a27190.png"
+  },
+  {
+    "revision": "da96770db4c6619ac7805fa8294f4680",
+    "url": "/static/media/3.da96770d.png"
+  },
+  {
+    "revision": "1710aaba5b8da1d23ec042549bd3788c",
+    "url": "/static/media/4.1710aaba.png"
+  },
+  {
+    "revision": "67476a6478e2ce85a516d69d5c6176fb",
+    "url": "/static/media/4.67476a64.png"
+  },
+  {
+    "revision": "813a0d3f7329f05cc202c43e0624a73c",
+    "url": "/static/media/4.813a0d3f.png"
+  },
+  {
+    "revision": "7106fb09bb63e00a76ee0d09b4491dca",
+    "url": "/static/media/5.7106fb09.png"
+  },
+  {
+    "revision": "a785b91ad424e4d66b9d214074a70ced",
+    "url": "/static/media/5.a785b91a.png"
+  },
+  {
+    "revision": "02c332397571feb10bd489cb4685c973",
+    "url": "/static/media/6.02c33239.png"
+  },
+  {
+    "revision": "4914973985c8da218c1c9a8de8c4ff10",
+    "url": "/static/media/6.49149739.png"
+  },
+  {
+    "revision": "ba637ef2a08730b504b601e81b5b976d",
+    "url": "/static/media/annikalundberg.ba637ef2.pdf"
+  },
+  {
+    "revision": "eea79c8e0e6d1a6abb47c1b44bd1031d",
+    "url": "/static/media/graphic.eea79c8e.png"
+  },
+  {
+    "revision": "d883c38807089a496e81dc34e951e663",
+    "url": "/static/media/instagramLogo.d883c388.png"
+  },
+  {
+    "revision": "eb7e54a6abb31e3578302b9c9e0bdcda",
+    "url": "/static/media/landingPage1.eb7e54a6.jpg"
+  },
+  {
+    "revision": "c3804d49dd0de2c80cc1b39d1599439e",
+    "url": "/static/media/linkedInLogo.c3804d49.png"
+  },
+  {
+    "revision": "9b06858ea452b61d28e2ec6b46c05858",
+    "url": "/static/media/livaLogo.9b06858e.jpg"
+  },
+  {
+    "revision": "b818d523d8560f33bcf5d6628f0ffa73",
+    "url": "/static/media/spotifyLogo.b818d523.png"
+  },
+  {
+    "revision": "982cf19f1120abae8b12cb645eda3147",
+    "url": "/static/media/thisIsMe1.982cf19f.jpg"
+  }
+]);
