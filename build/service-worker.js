ce820bd995087670c9d3776ff45818e8b9385241
lunann <annika.birgitta@hotmail.com>
2021-10-06 15:11:48 +0200

    -

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f90677e2e3c6d6e935ca0bb75d6a301.js"
+  "/precache-manifest.f65003fb74642df434283278c33c1412.js"
 );
 
 self.addEventListener('message', (event) => {
